b1d3aa58061288dd07f668b9aa032099f57d9182
OscarGonzalez1987 <55475821+OscarGonzalez1987@users.noreply.github.com>
2020-01-06 18:06:47 -0500

    Add files via upload

---
+++ librerias/js/interactividad.js
@@ -0,0 +1,83 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function(){
+    $("#a0b").hide();
+    $("#a1b").hide();
+    $("#a2b").hide();
+    
+    $("#a4b").hide();
+    $("#a5b").hide();
+    $("#a6b").hide();
+    $("#a7b").hide();
+    $("#a8b").hide();
+
+});
+
+$(document).ready(function(){
+    $("#a0a").click(function () {
+        $("#a0b").show();
+    });
+    $("#a0b").mouseout(function () {
+        $("#a0b").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#a1a").click(function () {
+        $("#a1b").show();
+    });
+    $("#a1b").mouseout(function () {
+        $("#a1b").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#a2a").click(function () {
+        $("#a2b").show();
+    });
+    $("#a2b").mouseout(function () {
+        $("#a2b").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#a4a").click(function () {
+        $("#a4b").show();
+    });
+    $("#a4b").mouseout(function () {
+        $("#a4b").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#a5a").click(function () {
+        $("#a5b").show();
+    });
+    $("#a5b").mouseout(function () {
+        $("#a5b").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#a6a").click(function () {
+        $("#a6b").show();
+    });
+    $("#a6b").mouseout(function () {
+        $("#a6b").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#a7a").click(function () {
+        $("#a7b").show();
+    });
+    $("#a7b").mouseout(function () {
+        $("#a7b").hide();
+    });
+});
+
+
